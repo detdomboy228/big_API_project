4b7b69a67044243c6c13541d468d66586bba5d58
detdomboy228 <94791820+detdomboy228@users.noreply.github.com>
2022-02-09 20:40:02 +0500

    exercise №1
    Надо ввести координаты места, с новой строчки каждую

---
+++ derba.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import pygame
+import requests
+
+a = input()
+b = input()
+z = '0.6,0.6'
+map_request = f"http://static-maps.yandex.ru/1.x/?ll={a},{b}&spn={z}&l=sat"
+response = requests.get(map_request)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+screen.blit(pygame.image.load(map_file), (0, 0))
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+os.remove(map_file)
