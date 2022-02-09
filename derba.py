c4f8294f0e77ff5d7aff5c8e848a0a6b4c0aa066
detdomboy228 <94791820+detdomboy228@users.noreply.github.com>
2022-02-09 20:44:16 +0500

    exercise №2
    -//-
    Управление через PgUp и PgDn

--- derba.py
+++ derba.py
@@ -5,25 +5,37 @@ import requests
 
 a = input()
 b = input()
-z = '0.6,0.6'
-map_request = f"http://static-maps.yandex.ru/1.x/?ll={a},{b}&spn={z}&l=sat"
-response = requests.get(map_request)
+z = 17
+map_request = f"http://static-maps.yandex.ru/1.x/?ll={a},{b}&z={z}&l=sat"
 
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
 
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+def bebra(map_r):
+    response = requests.get(map_r)
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    return map_file
+
 
 pygame.init()
+map = bebra(map_request)
 screen = pygame.display.set_mode((600, 450))
-screen.blit(pygame.image.load(map_file), (0, 0))
+screen.blit(pygame.image.load(map), (0, 0))
 pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
-pygame.quit()
-os.remove(map_file)
+while 3:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            os.remove(map)
+            sys.exit()
+        if e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_PAGEUP:
+                z += 1
+            elif e.key == pygame.K_PAGEDOWN:
+                z -= 1
+    map_request = f"http://static-maps.yandex.ru/1.x/?ll={a},{b}&z={z}&l=map"
+    map = bebra(map_request)
+    try:
+        screen.blit(pygame.image.load(map), (0, 0))
+    except Exception:
+        pass
+    pygame.display.flip()
