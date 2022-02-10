d85376936f8f96f6a317a3904d18816738ac9b93
Mal1gos <94733442+Mal1gos@users.noreply.github.com>
2022-02-10 20:37:25 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+import os
+import sys
+import pygame
+import requests
+
+a = float(input())
+b = float(input())
+z = 17
+map_request = f"http://static-maps.yandex.ru/1.x/?ll={a},{b}&z={z}&l=sat"
+
+
+# 123
+def bebra(map_r):
+    response = requests.get(map_r)
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    return map_file
+
+
+pygame.init()
+map = bebra(map_request)
+screen = pygame.display.set_mode((600, 450))
+screen.blit(pygame.image.load(map), (0, 0))
+pygame.display.flip()
+running = True
+while running:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            os.remove(map)
+            running = False
+        if e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_PAGEUP:
+                z += 1
+            if e.key == pygame.K_PAGEDOWN:
+                z -= 1
+            if e.key == pygame.K_RIGHT:
+                a += 1 / z ** 2
+            if e.key == pygame.K_LEFT:
+                a -= 1 / z ** 2
+            if e.key == pygame.K_DOWN:
+                b -= 1 / (z ** 2 + 10)
+            if e.key == pygame.K_UP:
+                b += 1 / (z ** 2 + 10)
+    map_request = f"http://static-maps.yandex.ru/1.x/?ll={a},{b}&z={z}&l=map"
+    map = bebra(map_request)
+    try:
+        screen.blit(pygame.image.load(map), (0, 0))
+    except Exception:
+        pass
+    pygame.display.flip()
+pygame.quit()
