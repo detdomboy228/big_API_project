277d4fb512c5027f025ca539da525fe92867aca7
Mal1gos <94733442+Mal1gos@users.noreply.github.com>
2022-02-11 18:24:32 +0500

    Add files via upload
    №8!!!

--- main.py
+++ main.py
@@ -2,11 +2,40 @@ import os
 import sys
 import pygame
 import requests
-
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
 a = float(input())
 b = float(input())
 z = 17
+pts = []
 map_request = f"http://static-maps.yandex.ru/1.x/?ll={a},{b}&z={z}&l=sat"
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('bebrar.ui', self)
+        self.setWindowTitle('       ПАНЕЛЬ УПРАВЛЕНИЯ')
+        self.btn.clicked.connect(self.run)
+        self.btn2.clicked.connect(self.delete)
+
+    def run(self):
+        global a, b, pts
+        adress = self.name_l.text()
+        result, adress_doma = str(find_coords(adress)[0]), str(find_coords(adress)[1])
+        self.label_3.setText(adress_doma.split(', ')[-2])
+        self.label_4.setText(adress_doma.split(', ')[-1])
+        if result != 'None':
+            a = float(result.split()[0])
+            b = float(result.split()[-1])
+            pts.append(f'{a},{b}')
+
+    def delete(self):
+        global pts
+        del pts[-1]
+        self.name_l.setText('')
+        self.label_3.setText('')
+        self.label_4.setText('')
 
 
 # 123
@@ -18,17 +47,33 @@ def bebra(map_r):
     return map_file
 
 
+def find_coords(adr):
+    geocoder_request = f"""http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={adr}&format=json"""
+    response = requests.get(geocoder_request)
+    try:
+        json_response = response.json()
+        toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+        toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
+        toponym_coodrinates = toponym["Point"]["pos"]
+        return toponym_coodrinates, toponym_address
+    except Exception:
+        return
+
+
 pygame.init()
-map = bebra(map_request)
+mapp = bebra(map_request)
 screen = pygame.display.set_mode((600, 450))
-screen.blit(pygame.image.load(map), (0, 0))
+screen.blit(pygame.image.load(mapp), (0, 0))
 pygame.display.flip()
 running = True
 mode = 'sat'
+app = QApplication(sys.argv)
+ex = MyWidget()
+ex.show()
 while running:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
-            os.remove(map)
+            os.remove(mapp)
             running = False
         if e.type == pygame.KEYDOWN:
             if e.key == pygame.K_PAGEUP:
@@ -51,11 +96,17 @@ while running:
                 mode = 'sat,skl'
     screen.fill(pygame.Color('white'))
     map_request = f"http://static-maps.yandex.ru/1.x/?ll={a},{b}&z={z}&l={mode}"
-    map = bebra(map_request)
+    if pts:
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={a},{b}&z={z}&l={mode}&pt="
+    for e in pts:
+        if e != pts[-1]:
+            map_request += e + '~'
+        else:
+            map_request += e
+    mapp = bebra(map_request)
     try:
-        screen.blit(pygame.image.load(map), (0, 0))
+        screen.blit(pygame.image.load(mapp), (0, 0))
     except Exception:
         pass
-
     pygame.display.flip()
 pygame.quit()
